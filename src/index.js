62ef7cab99a79bb444c6b5afaea2ba3564c1b648
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-02-25 00:29:14 +0300

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 module.exports = function reverse (n) {
-        return (
-          parseFloat(
-            n.toString(),
-            n.split(''),
-            n.reverse(),
-            n.join(''),
-          ) * Math.sign(n)
-        )                 
+    let s = n.toString();
+    let a = Array.from(s);
+    let a1 = [];
+    for (let i = a.length - 1; i >= 0; i--) {
+        a1.push(a[i]);
+    }
+    const r = a1.join('');
+    if (r.includes('-')){
+        return r.slice(0,3);
+    } else {
+        return r;
+    }
+    
 }
