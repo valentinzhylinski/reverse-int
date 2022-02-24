f262161224e71be08003dd6146ecef1893729b65
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-02-24 22:55:17 +0300

    add: add function?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+        return (
+          parseFloat(
+            n.toString(),
+            n.split(''),
+            n.reverse(),
+            n.join(''),
+          ) * Math.sign(n)
+        )                 
 }
